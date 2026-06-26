8d32909f12b9a9f21d335d4b268970a0ab975c84
Kashi Vishwanath <kashi.vishwanath6254@gmail.com>
2026-06-26 14:28:34 +0530

    refactor(links): extract link buisness logic into service layer

--- backend/src/controllers/redirect.controller.ts
+++ backend/src/controllers/redirect.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import linkStore from "../data/storage.js";
+import { linkService } from "../services/links.service.js";
 
 export const redirectSlug = (req: Request, res: Response) => {
   try {
@@ -7,11 +7,11 @@ export const redirectSlug = (req: Request, res: Response) => {
     if (Array.isArray(slug)) {
       return res.status(400).json({ message: "slug type not valid" });
     }
-    if (!linkStore[slug]) {
+    const result = linkService.getDestinationUrl(slug);
+    if (!result) {
       return res.status(404).json({ message: "No link found" });
     }
-    const redirectURL = linkStore[slug].destinationUrl;
-    return res.redirect(redirectURL);
+    return res.redirect(result);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
