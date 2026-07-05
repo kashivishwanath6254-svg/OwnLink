5bbeeda32179347d3bc50892fb8464a93e9fd6c5
Kashi Vishwanath <kashi.vishwanath6254@gmail.com>
2026-07-05 09:28:28 +0530

    feat: set up PostgresSQL connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
 import app from "./app.js";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { verifyDatabaseConnection } from "./db/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/`);
-});
+verifyDatabaseConnection()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to PostgresSQL:", err);
+  });
